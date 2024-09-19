db117e6d138eed80ee83a291f173438008f77882
Hunter <hunvicter@gmail.com>
2024-09-19 23:51:40 +0700

    feat:home-part2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Hero } from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import BestSeller from '../components/BestSeller'
+import PurPolicy from '../components/PurPolicy'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 function Home() {
   return (
     <div>
       <Hero/>
     <LatestCollection/>
+    <BestSeller/>
+    <PurPolicy/>
+    <NewsLetterBox/>
     </div>
   )
 }
