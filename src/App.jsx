400cdb1a3da5145b5df9250a45957463e3ea9c9b
Hunter <hunvicter@gmail.com>
2024-09-27 00:09:51 +0700

    feat:collection

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,15 @@ import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
-import Collections from './pages/Collections'
+import SearchBar from './components/SearchBar'
 const App = () => {
   return (
     <div className=' px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]' >
       <NavBar/>
+      <SearchBar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route paht='/collection' element={<Collection/>} />
+      <Route path='/collection' element={<Collection/>} />
       <Route path='/about' element={<About/>} />
       <Route path='/contact' element={<Contact/>} />
       <Route path='/product/:productId' element={<Product/>}/>
