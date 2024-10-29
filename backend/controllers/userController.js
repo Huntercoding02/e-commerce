985a81bdd20ff690799cfdb6b7a07753ec53c9fa
Hunter <hunvicter@gmail.com>
2024-10-29 11:44:35 +0700

    feat : create controller , models and routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,19 @@
+
+//Route for user login
+const loginUser = async ()=>{
+
+
+
+}
+
+//Route for user register
+const registerUser = async (req,res) =>{
+
+}
+
+// Route for admin login
+const adminLogin = async ()=>{}
+
+
+
+export {loginUser,registerUser,adminLogin}
