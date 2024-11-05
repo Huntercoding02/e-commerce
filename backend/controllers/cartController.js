f28f1a320a63486b202e0ff3aee71470dba3054a
Hunter <hunvicter@gmail.com>
2024-11-05 15:47:39 +0700

    feat : add logic to navbar to work properly

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,18 @@
+
+
+// add products to user cart
+const addToCart = async (req,res)=>{
+
+}
+
+// update products to user cart
+const updateToCart = async (req,res)=>{
+    
+}
+
+// get user cart data
+const setUserCart = async (req,res)=>{
+    
+}
+
+export { addToCart,updateToCart,setUserCart}
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -10,10 +10,11 @@ const NavBar = () => {
   
   
   const logout = ()=>{
+    navigate('/login')
     localStorage.removeItem('token')
     setToken('')
     setCartItems({})
-    navigate('/login')
+    
   }
 
   return (
@@ -45,20 +46,26 @@ const NavBar = () => {
       </ul>
       <div className="flex items-center gap-6">
         <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        
+       
         <div className="group relative">
-          <Link to={'/login'}><img
+          <img
+          onClick={()=> token ? null : navigate('/login')}
             className="w-5 cursor-pointer"
             src={assets.profile_icon}
             alt=""
-          /></Link>
-          <div className="group-hover:block hidden absolute gropdown-menu right-0 pt-4 ">
+          />
+          {/* Dropdown Menu */}
+          {token && <div className="group-hover:block hidden absolute gropdown-menu right-0 pt-4 ">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My profile</p>
-              <p className="cursor-pointer hover:text-black">Orders</p>
-              <p className="cursor-pointer hover:text-black">Logout</p>
+              <p onClick={()=>navigate('/orders')} className="cursor-pointer hover:text-black">Orders</p>
+              <p onClick={logout} className="cursor-pointer hover:text-black">Logout</p>
             </div>
-          </div>
+          </div>}
         </div>
+
+        {/* */}
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
