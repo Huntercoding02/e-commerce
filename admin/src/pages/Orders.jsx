6d7c6ede397421aa2628d26d77162d53fbf3ee8b
Hunter <hunvicter@gmail.com>
2024-11-07 10:25:32 +0700

    feat : add API on admin panel page

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import {backendUrl} from '../App'
+import { toast } from 'react-toastify'
 
-const Orders = () => {
+const Orders = ({token}) => {
+  
+  const [orders,setOrders] = useState([])
+
+  const fetchAllOrders = async ()=>{
+    if (!token) {
+      return null
+    }
+    try {
+      const response = await axios.post(backendUrl +'/api/order/list',{},{headers:{token}})
+      if (response.data.success) {
+        setOrders(response.data.orders);
+      }else{
+        toast.error(response.data.message)
+      }
+      
+      
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+
+  }
+
+  useEffect(()=>{
+    fetchAllOrders()
+  },[token])
   return (
-    <div>Orders</div>
+    <div>
+
+    </div>
   )
 }
 
